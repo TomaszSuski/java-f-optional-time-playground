2397bb51b510f6577ca91bc7f0db299ffea95a5c
Tomasz Suski <tomasz.suski@orba.co>
2024-04-16 16:03:37 +0200

    using Record class to store Person type records

--- src/main/java/generics/Repository.java
+++ src/main/java/generics/Repository.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Repository<T> {
+
+    record Person(String firstName, String lastName) {}
     private List<T> records = new ArrayList<>();
 
     List<T> findAll() {
@@ -27,5 +29,11 @@ public class Repository<T> {
         repo.save("road");
 
         System.out.println(repo.findAll());
+
+        Repository<Person> pRepo = new Repository<>();
+        pRepo.save(new Person("John", "Doe"));
+        pRepo.save(new Person("Jane", "Doe"));
+
+        System.out.println(pRepo.findAll());
     }
 }
